a5f8679ffc8567e9214bcc72e6b8f5d853e4f6cd
emrecore <emre@blgn.de>
2025-08-21 15:56:36 +0200

    Added the prices for later calculations

--- TheCode/TheCode.py
+++ TheCode/TheCode.py
@@ -1,3 +1,11 @@
+#Prices
+bubblegum = 2.0
+toffee = 0.2
+ice_cream = 5.0
+milk_chocolate = 4.0
+doughnut = 2.5
+pancake = 3.2
+
 #Introduction
 print("Welcome to the shop!")
 print()
@@ -9,3 +17,5 @@ print("Ice cream: $5")
 print("Milk chocolate: $4")
 print("Doughnut: $2.5")
 print("Pancake: $3.2")
+print()
+
