a546829634eca7b572de6a62779ec9f68a8770fe
emrecore <emre@blgn.de>
2025-08-21 16:20:32 +0200

    Added simple one-time purchase system

--- TheCode/TheCode.py
+++ TheCode/TheCode.py
@@ -19,3 +19,41 @@ print("Doughnut: $2.5")
 print("Pancake: $3.2")
 print()
 
+cost = 0.0
+what = input("Please tell me what you want to purchase: ")
+
+if what == "Bubblegum":
+    amount = int(input("Please tell me how much bubblegum to bubble: "))
+    bubblegum = amount * bubblegum
+    cost = cost + bubblegum
+
+if what == "Toffee":
+    amount = int(input("Please tell me how much toffee: "))
+    toffee = amount * toffee
+    cost = cost + toffee
+
+if what == "Ice cream":
+    amount = int(input("Please tell me how much ice cream: "))
+    ice_cream = amount * ice_cream
+    cost = cost + ice_cream
+
+if what == "Milk chocolate":
+    amount = int(input("Please tell me how much milk chocolate: "))
+    milk_chocolate = amount * milk_chocolate
+    cost = cost + milk_chocolate
+
+if what == "Doughnut":
+    amount = int(input("Please tell me how much doughnut: "))
+    doughnut = amount * doughnut
+    cost = cost + doughnut
+
+if what == "Pancake":
+    amount = int(input("Please tell me how much pancake: "))
+    pancake = amount * pancake
+    cost = cost + pancake
+
+
+print("This will make:", cost, "dollars.")
+print()
+print("Have a nice day!")
+
